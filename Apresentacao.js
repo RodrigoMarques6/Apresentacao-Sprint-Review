bf44d16985e4a9da502ae99b3bf7a9173246f112
RodrigoMarques <rodrigomarques328@hotmail.com>
2022-11-22 18:51:38 -0300

    Apresentacao

--- Apresentacao.js
+++ Apresentacao.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM está funcionando corretamente.')
-    openDivLivro()
+    openDivLivro();
+    openDivGerente();
+    openDivFluxos();
+    openDivCodigos();
+    openDivConclusao();
 });
 
 
@@ -11,6 +15,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // setAttribute('style', 'display: none')
 // Realizar condicional 
+
+
 
 // const sectionApresentacao = document.getElementById('section-apresentacao')
 const divLivro = document.getElementById('div-livro')
@@ -29,5 +35,64 @@ function openDivLivro() {
   }
 
 
+  const h1Gerente = document.getElementById('h1-gerente')
+  const divGerente = document.getElementById('div-gerente')
 
+  function openDivGerente() {
+    $("#h1-gerente").on("click", function () {
+      $("#div-gerente").slideToggle("slow");
+    });
+  
+    $("#algoaqui").each(function () {
+      $(this).on("click", function () {
+        $("#div-gerente").slideToggle("slow");
+      });
+    });
+  }
+
+  const h1Fluxos = document.getElementById('h1-fluxos')
+  const divFluxos = document.getElementById('div-fluxos')
+
+  function openDivFluxos() {
+    $("#h1-fluxos").on("click", function () {
+      $("#div-fluxos").slideToggle("slow");
+    });
+  
+    $("#algoaqui").each(function () {
+      $(this).on("click", function () {
+        $("#div-fluxos").slideToggle("slow");
+      });
+    });
+  }
+
+  const h1Codigos = document.getElementById('h1-codigos')
+  const divCodigos = document.getElementById('div-codigos')
+
+
+  function openDivCodigos() {
+    $("#h1-codigos").on("click", function () {
+      $("#div-codigos").slideToggle("slow");
+    });
+  
+    $("#algoaqui").each(function () {
+      $(this).on("click", function () {
+        $("#div-codigos").slideToggle("slow");
+      });
+    });
+  }
+
+  const h1Conclusao = document.getElementById('h1-conclusao')
+  const divConclusao = document.getElementById('div-conclusao')
+
+  function openDivConclusao() {
+    $("#h1-conclusao").on("click", function () {
+      $("#div-conclusao").slideToggle("slow");
+    });
+  
+    $("#algoaqui").each(function () {
+      $(this).on("click", function () {
+        $("#div-conclusao").slideToggle("slow");
+      });
+    });
+  }
 
