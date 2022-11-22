ba7a9512e9f9e2efe14874a4191fba14d142b760
RodrigoMarques <rodrigomarques328@hotmail.com>
2022-11-22 17:59:33 -0300

    Apresentacao

--- Apresentacao.js
+++ Apresentacao.js
@@ -2,3 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM está funcionando corretamente.')
 });
 
+const spanFluxos = document.querySelector('#span-fluxos')
+const divFluxos = document.querySelector('#div-fluxos')
+
+// spanFluxos.addEventListener('click', function () {
+//     divFluxos.style.display = 'none'
+// })
+
+// setAttribute('style', 'display: none')
+// Realizar condicional 
+
+
+
+
